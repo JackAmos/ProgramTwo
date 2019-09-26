d7ff734fef697a53915eb6044d483b917b5443f3
Jack Amos <jack671guam@yahoo.com>
2019-09-26 13:34:20 -0600

    Can draw box and line

--- program_2.py
+++ program_2.py
@@ -13,29 +13,71 @@ def print_coordinates(filename):
 	#get each value in string an convert it to integer
 	# [llx,lly,d,p1,p2,v1,v2]
 	coord_values = [int(x) for x in contents.split()]
+
+	#sets scale of "screen"
+	dimension = 2*coord_values[2]
 	
 	#2d array that will be printed
-	screen = [["*"]*30]*30
-	y_axis = "|"
-	x_axis = "_"
+	screen = [[" "]*dimension for x in range(dimension)]
 
-	implcit = get_implicit(coord_values)
+	implicit = get_implicit(coord_values)
+
+	#plot box 1st point
+	y_point = dimension - abs(coord_values[1])
+	x_point = abs(coord_values[0])
+
+	#exists to make array index not all = 0
+	index = 0
+	for n in screen:
+		screen[index][index] = "w"
+		index += 1
+
+	r1 = range((y_point-coord_values[2]+1),y_point+1)
+	index = x_point
+
+	#make actual box
+	for n in screen:
+		if screen.index(n) in r1:
+			n[x_point] = "*"
+			n[x_point+coord_values[2]] = "*"
+		if screen.index(n) == y_point or screen.index(n) == (y_point-coord_values[2]+1):
+			while index < (x_point+coord_values[2]):
+				n[index] = "*"
+				index+=1
+			index = x_point
+
+	row_index = 0
+	column_index = 0
+
+	#draw line
+	while index <= dimension:
+		if (implicit[0]*row_index+implicit[1]*column_index+implicit[2]) == 0:
+			screen[row_index][column_index] = "*"
+
 
 
 	row = 0
 	screen_string = ""
 
-	while row < 30:
+	while row < dimension:
 		#adds row of values to output string plus newline char
-		screen_string += str(screen[row]) + "\n"
+		screen_string += "|" + str(screen[row]) + "\n"
 		#increment by column number so vars are at the start and end of next row
 		row+=1
 
-	#removes commas and brackets for beautification
-	screen_string = screen_string.replace(",","").replace("[","").replace("]","").replace("'","")
-	print(screen_string)
-	
+	#adds x axis to the bottom of the graph
+	x_axis = ["_"]*dimension
+	screen_string+= "|" + str(x_axis)
 
+	#removes unneeded chars for beautification
+	screen_string = screen_string.replace(",","").replace("[","").replace("]","").replace("'","").replace("w"," ")	
+
+
+
+	#print final graph
+	print(screen_string)
+	print("\n")
+	print("Implicit Form:	"+str(implicit[0])+"x+"+str(implicit[1])+"x+"+str(implicit[2])+"=0")
 
 
 
